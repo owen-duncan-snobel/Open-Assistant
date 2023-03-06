f04741e0778c7fca68e07a33c641988494e97ede
Owen Duncan-Snobel <owenduncansnobel@gmail.com>
2023-03-05 20:30:29 -0500

    fix for invalid avatarUrl

--- website/src/components/UserAvatar.tsx
+++ website/src/components/UserAvatar.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { ImgHTMLAttributes, SyntheticEvent } from "react";
 export function UserAvatar(options: { displayName: string; avatarUrl: string | null }) {
   const { displayName, avatarUrl } = options;
   return (
@@ -8,6 +9,9 @@ export function UserAvatar(options: { displayName: string; avatarUrl: string | n
           avatarUrl
             ? avatarUrl
             : `https://api.dicebear.com/5.x/initials/png?seed=${displayName}&radius=50&backgroundType=gradientLinear`
+        }
+        onError={(event: SyntheticEvent<HTMLImageElement, Event>) =>
+          (event.currentTarget.src = `https://api.dicebear.com/5.x/initials/png?seed=${displayName}&radius=50&backgroundType=gradientLinear`)
         }
         alt={`${displayName}'s avatar`}
         width={30}
